8a339236f45cea16b0053eeaef56a7a4e9526965
Bonsuck Koo <bonsuckkoo@gmail.com>
2025-02-04 20:57:01 -0600

    start coding repo

---
+++ javascript/Exercise/index.js
@@ -0,0 +1,38 @@
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown() 
+//function fortytwo() {
+//     console.log(42)
+// }
+
+// fortytwo( )
+
+
+// let lap1 = 34
+// let lap2 = 33 
+// let lap3 = 36
+
+// function totallaptime () {
+//     console.log(lap1 + lap2 + lap3)
+// }
+
+// totallaptime()
+
+let lapsCompleted = 0
+
+function incrementLap() {
+    lapsCompleted = lapsCompleted +1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+
+
+console.log(lapsCompleted)
---
+++ javascript/index.js
@@ -0,0 +1,71 @@
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let count = firstBatch
+// console.log(count)
+// let myAge = 24
+// console.log(myAge)
+
+////////////////////////////////////////////////////////////////
+// let myAge = 24
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// let count = 5
+////////////////////////////////////////////////////////////
+//count + 1
+// count = count + 2
+
+
+// count = count + 10
+
+// count = count - 2
+
+
+// console.log(count)
+
+// let bonuspoints = 50
+// console.log(bonuspoints)
+
+// bonuspoints = bonuspoints + 50
+// console.log(bonuspoints)
+
+// bonuspoints = bonuspoints - 75
+// console.log(bonuspoints)
+
+// bonuspoints = bonuspoints +45
+// console.log(bonuspoints)
+//////////////////////////////////////////////////////////////
+
+
+//initialize the ocunt as 0
+// listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
+
+// function increment() {
+//     console.log("the button was clicked")
+// }
+
+
+
+let count = 0
+let countEl = document.getElementById("count-el")
+function increment() {
+    count = count + 1
+    countEl.innerText = count 
+}
+
+
+
+// let count = 0
+
+// function increment() {
+//     count = count +1
+//     document.getElementById(".count-el").innerHTML = count
+//     console.log(document.getElementById(".count-el"))
+// }
+
